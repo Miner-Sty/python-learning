935fa4560945104b78379a57bb6d814a3fe33146
Miner-Sty <874155706@qq.com>
2015-07-11 09:50:35 +0800

    e-mail encrypt

---
+++ email/sendemail_encrypt.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__='Miner'
+
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( \
+        Header(name, 'utf-8').encode(), \
+        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+from_addr = raw_input('QQMail: ')
+password = raw_input('Password: ')
+to_addr = raw_input('To: ')
+smtp_server = 'smtp.qq.com'
+smtp_port = 25
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr(u'Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
+msg['Subject'] = Header(u'来自SMTP的问候……', 'utf-8').encode()
+
+server = smtplib.SMTP(smtp_server, smtp_port)
+server.starttls()
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
